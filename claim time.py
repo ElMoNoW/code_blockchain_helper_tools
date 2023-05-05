a4e8ebb95e66d29a08be2eadb154351198aec6ba
ElMoNoW <75977599+ElMoNoW@users.noreply.github.com>
2023-05-05 21:46:11 +0700

    Create claim time.py

---
+++ claim time.py
@@ -0,0 +1,152 @@
+#############################
+## Helper Tools
+## Made by BoboCoin 
+## (t)ime to claim 
+## writing in python on Android 
+#############################
+##
+## Calculate the Fungible Tokens get per hour to day week month year and see the Output...This program is used to estimate the proportion of Guild_Token_Tools which give Guild Tokens ROCK SCISSOR PAPER per hour for Fixed Rate ...
+## Feel free to modify and use it yourself 
+##
+#############################
+##
+## See this Code on Blockchain #bp0x0
+## Collection: bobopixeloxo
+## Schemes: rock.oxo - scissor.oxo - paper.oxo
+## Stake at  waxdao.io: rock.oxo - scissor.oxo - paper.oxo - bobopixeloxo 
+##
+#############################
+##
+##DELETE (#) TO ACTIVE LINE!!!
+##DO NOT DELETE (##) Info Lines!!!
+##
+##
+##
+##
+print('Fungible Token Per Hour To 50 Years Generation...1\n')
+#############################
+## 1oo  FTs in 24h 4.1667 - 8.3334 - 16.6668
+##DELETE (#) TO ACTIVE LINE!!!######
+## or let it as it is with standard Value ##
+#############################
+#print("How many FTs for Lv_1 per Hour ")
+#lv1 = int(input())
+#print("How many FTs for Lv_2 per Hour ")
+#lv2 = int(input())
+#print("How many FTs for Lv_3 per Hour ")
+#lv3 = int(input())
+#
+try:
+	lv1 = lv1
+except:
+	lv1 = 4.1667
+try:
+	lv2 = LV2
+except:
+	lv2 = 8.3334
+try:
+	lv3 = lv3
+except:
+	lv3 = 16.6668
+print(f'___Calc___1h\n\n1 h\nlv1 = {lv1}\nlv2 = {lv2}\nlv3 = {lv3}\n')
+lv1 *= 24
+lv2 *= 24
+lv3 *= 24
+print(f'___Calc___24h\n\n24 h\nlv1 = {lv1}\nlv2 = {lv2}\nlv3 = {lv3}\n')
+lv1 *= 7
+lv2 *= 7
+lv3 *= 7
+print(f'___Calc___7d\n\n7 d\nlv1 = {lv1}\nlv2 = {lv2}\nlv3 = {lv3}\n')
+lv1 *= 4
+lv2 *= 4
+lv3 *= 4
+print(f'___Calc___1m\n\n1 m\nlv1 = {lv1}\nlv2 = {lv2}\nlv3 = {lv3}\n')
+lvv1 = lv1 * 2
+lvv2 = lv2 * 2
+lvv3 = lv3 * 2
+print(f'___Calc___2m\n\n2 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 3
+lvv2 = lv2 * 3
+lvv3 = lv3 * 3
+print(f'___Calc___3m\n\n3 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 4
+lvv2 = lv2 * 4
+lvv3 = lv3 * 4
+print(f'___Calc___4m\n\n4 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 5
+lvv2 = lv2 * 5
+lvv3 = lv3 * 5
+print(f'___Calc___5m\n\n5 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 6
+lvv2 = lv2 * 6
+lvv3 = lv3 * 6
+print(f'___Calc___6m\n\n6 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 7
+lvv2 = lv2 * 7
+lvv3 = lv3 * 7
+print(f'___Calc___7m\n\n7 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 8
+lvv2 = lv2 * 8
+lvv3 = lv3 * 8
+print(f'___Calc___8m\n\n8 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 9
+lvv2 = lv2 * 9
+lvv3 = lv3 * 9
+print(f'___Calc___9m\n\n9 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 10
+lvv2 = lv2 * 10
+lvv3 = lv3 * 10
+print(f'___Calc___10m\n\n10 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 11
+lvv2 = lv2 * 11
+lvv3 = lv3 * 11
+print(f'___Calc___11m\n\n11 m\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvv1 = lv1 * 12
+lvv2 = lv2 * 12
+lvv3 = lv3 * 12
+print(f'___Calc___1y\n\n1 y\nlv1 = {lvv1}\nlv2 = {lvv2}\nlv3 = {lvv3}\n')
+lvvv1 = lvv1 * 2
+lvvv2 = lvv2 * 2
+lvvv3 = lvv3 * 2
+print(f'___Calc___2y\n\n2 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 3
+lvvv2 = lvv2 * 3
+lvvv3 = lvv3 * 3
+print(f'___Calc___3y\n\n3 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 4
+lvvv2 = lvv2 * 4
+lvvv3 = lvv3 * 4
+print(f'___Calc___4y\n\n4 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 5
+lvvv2 = lvv2 * 5
+lvvv3 = lvv3 * 5
+print(f'___Calc___5y\n\n5 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 6
+lvvv2 = lvv2 * 6
+lvvv3 = lvv3 * 6
+print(f'___Calc___6y\n\n6 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 7
+lvvv2 = lvv2 * 7
+lvvv3 = lvv3 * 7
+print(f'___Calc___7y\n\n7 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 8
+lvvv2 = lvv2 * 8
+lvvv3 = lvv3 * 8
+print(f'___Calc___8y\n\n8 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 9
+lvvv2 = lvv2 * 9
+lvvv3 = lvv3 * 9
+print(f'___Calc___9y\n\n9 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 10
+lvvv2 = lvv2 * 10
+lvvv3 = lvv3 * 10
+print(f'___Calc___10y\n\n10 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 11
+lvvv2 = lvv2 * 11
+lvvv3 = lvv3 * 11
+print(f'___Calc___20y\n\n20 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+lvvv1 = lvv1 * 50
+lvvv2 = lvv2 * 50
+lvvv3 = lvv3 * 50
+print(f'___Calc___50y\n\n50 y\nlv1 = {lvvv1}\nlv2 = {lvvv2}\nlv3 = {lvvv3}\n')
+print ('...')
